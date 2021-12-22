7f1340d4837a0fef858c1245be83a79668c06848
Listen-Y <1604053140@qq.com>
2021-12-22 12:08:20 +0800

    学习channel

--- 20211216/demo.go
+++ 20211216/demo.go
@@ -1,13 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
-func main() {
+func main1() {
 	var chanInt chan int
 	chanInt = make(chan int, 2)
 	chanInt <- 1
 	chanInt <- 2
-	//(chanInt)
+	close(chanInt)
 	for i := 0; i < 4; i++ {
 		num, ok := <-chanInt
 		fmt.Println(num)
@@ -15,4 +18,18 @@ func main() {
 		// 如果这个管道没有关闭，此时管道缓冲区也没有数据，编译器检查也没有协程往这个管道加数据，那么就会发生死锁deadklock
 		fmt.Println(ok)
 	}
+	num := 109
+	typeOf := reflect.TypeOf(num)
+	name := typeOf.Name()
+	fmt.Println(name)
+}
+
+func main() {
+	var num = 10
+	//value := reflect.ValueOf(num)
+	//value.SetInt(20) //这样操作会报错,如果要进行set,必须处理的是由指针带来的value
+	valueOf := reflect.ValueOf(&num)
+	valueOf.Elem().SetInt(20)
+
+	fmt.Println(num)
 }
