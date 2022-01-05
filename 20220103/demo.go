9469d1a0fe6183f481b2cf99225ea964326c3f1b
Listen-Y <1604053140@qq.com>
2022-01-05 12:39:03 +0800

    管道基本使用

--- 20220103/demo.go
+++ 20220103/demo.go
@@ -95,6 +95,80 @@ func main4() {
 	fmt.Println(ret)
 }
 
-func main() {
+func main5() {
+	intCh := make(chan int, 3)
 
+	fmt.Printf("intCh的值: %v, intCh的类型: %T, intCh的地址: %p\n", intCh, intCh, &intCh)
+	// intCh的值: 0xc00007e000, intCh的类型: chan int, intCh的地址: 0xc000006028
+	close(intCh)
+}
+
+func main6() {
+	intCh := make(chan int, 3)
+	intCh <- 1
+	intCh <- 2
+	intCh <- 3
+	close(intCh)
+	//for ch := range intCh {
+	//	fmt.Println(ch)
+	//}
+	for true {
+		v, ok := <-intCh
+		if !ok {
+			break
+		}
+		fmt.Println(v)
+	}
+}
+
+var dataMap = make(map[int]uint64, 20)
+
+func main7() {
+	intCh := make(chan int, 20)
+	exitCh := make(chan bool, 1)
+	go writeData(intCh)
+	go readData(intCh, exitCh)
+
+	// 下面这俩种写法都可以，也没有什么大区别
+	/*	for true {
+			v, ok := <- exitCh
+			if !ok {
+				break
+			}
+		fmt.Println(v)
+		}*/
+
+	for ch := range exitCh {
+		if ch {
+			break
+		}
+	}
+
+	fmt.Println(dataMap)
+}
+
+func writeData(intCh chan int) {
+	for i := 1; i <= 20; i++ {
+		intCh <- i
+	}
+	// 如果管道不再需要放入数据就及时关闭
+	close(intCh)
+}
+
+func readData(intCh chan int, exitCh chan bool) {
+	for true {
+		num, ok := <-intCh
+		if !ok {
+			break
+		}
+		// 计算
+		var ret uint64 = 1 //防止数据放不下
+		for i := 2; i <= num; i++ {
+			ret = ret * uint64(i)
+		}
+		dataMap[num] = ret
+	}
+	// 计算完成后退出
+	exitCh <- true
+	close(exitCh)
 }
