3e324c7feaa439dd8fbd3fc1295cf9a9650104c8
Listen-Y <1604053140@qq.com>
2022-04-20 23:21:00 +0800

    url参数获取

--- 0_go_frame_study/go_gin/demo.go
+++ 0_go_frame_study/go_gin/demo.go
@@ -4,9 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"strings"
 )
 
-func main() {
+func main1() {
 	// 1.创建路由
 	r := gin.Default()
 	// 2.绑定路由规则，执行的函数
@@ -25,4 +26,24 @@ func main() {
 		log.Fatalln("error")
 	}
 	log.Println("success")
+}
+
+// 可以通过Context的Param方法来获取API参数
+func main2() {
+	r := gin.Default()
+	r.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		//截取/
+		action = strings.Trim(action, "/")
+		c.String(http.StatusOK, name+" is "+action)
+	})
+	//默认为监听8080端口
+	r.Run(":8000")
+}
+
+// URL参数可以通过DefaultQuery()或Query()方法获取
+//DefaultQuery()若参数不存在，返回默认值，Query()若不存在，返回空串
+func main() {
+
 }
