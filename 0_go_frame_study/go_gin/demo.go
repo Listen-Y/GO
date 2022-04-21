d67e6f2a200ca97be64daaef9c454529819ee877
Listen-Y <1604053140@qq.com>
2022-04-21 23:32:16 +0800

    gin 文件、路由group、表单、参数

--- 0_go_frame_study/go_gin/demo.go
+++ 0_go_frame_study/go_gin/demo.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
@@ -44,6 +45,89 @@ func main2() {
 
 // URL参数可以通过DefaultQuery()或Query()方法获取
 //DefaultQuery()若参数不存在，返回默认值，Query()若不存在，返回空串
-func main() {
+func main3() {
+	r := gin.Default()
+	r.GET("/user", func(c *gin.Context) {
+		//指定默认值
+		//http://localhost:8080/user 才会打印出来默认的值 枯藤
+		// 如果是user?name=lxy 会打印出来lxy
+		name := c.DefaultQuery("name", "枯藤")
+		c.String(http.StatusOK, fmt.Sprintf("hello %s", name))
+	})
+	r.Run(":8000")
+}
 
+// 接受form表单中的数据
+func main4() {
+	r := gin.Default()
+	r.POST("/form", func(c *gin.Context) {
+		types := c.DefaultPostForm("type", "post")
+		username := c.PostForm("username")
+		password := c.PostForm("userpassword")
+		// c.String(http.StatusOK, fmt.Sprintf("username:%s,password:%s,type:%s", username, password, types))
+		c.String(http.StatusOK, fmt.Sprintf("username:%s,password:%s,type:%s", username, password, types))
+	})
+	r.Run(":8000")
+}
+
+// 上传多个文件
+func main5() {
+	// 1.创建路由
+	// 默认使用了2个中间件Logger(), Recovery()
+	r := gin.Default()
+	// 限制表单上传大小 8MB，默认为32MB
+	r.MaxMultipartMemory = 8 << 20
+	r.POST("/upload", func(c *gin.Context) {
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("get err %s", err.Error()))
+		}
+		if form == nil {
+			panic("form can not be nil")
+		}
+		// 获取所有图片
+		files := form.File["files"]
+		// 遍历所有图片
+		for _, file := range files {
+			// 逐个存
+			if err := c.SaveUploadedFile(file, file.Filename); err != nil {
+				c.String(http.StatusBadRequest, fmt.Sprintf("upload err %s", err.Error()))
+				return
+			}
+		}
+		c.String(200, fmt.Sprintf("upload ok %d files", len(files)))
+	})
+	//默认端口号是8080
+	r.Run(":8000")
+}
+
+// routes group是为了管理一些相同的URL
+// gin的helloWorld
+func main() {
+	// 1.创建路由
+	// 默认使用了2个中间件Logger(), Recovery()
+	r := gin.Default()
+	// 路由组1 ，处理GET请求
+	v1 := r.Group("/v1")
+	// {} 是书写规范
+	{
+		v1.GET("/login", login)
+		v1.GET("submit", submit)
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.POST("/login", login)
+		v2.POST("/submit", submit)
+	}
+	r.Run(":8000")
+}
+
+func login(c *gin.Context) {
+	name := c.DefaultQuery("name", "jack")
+	c.String(200, fmt.Sprintf("hello %s\n", name))
+}
+
+func submit(c *gin.Context) {
+	name := c.DefaultQuery("name", "lily")
+	c.String(200, fmt.Sprintf("hello %s\n", name))
 }
